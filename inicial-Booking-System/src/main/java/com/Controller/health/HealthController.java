6f52f7909cdfe696623edb0df3755111afea732f
Cristopher <cristopherquintanilla20060@gmail.com>
2024-07-06 15:24:43 -0600

    configuración inicial Booking System REST API.

--- inicial-Booking-System/src/main/java/com/Controller/health/HealthController.java
+++ inicial-Booking-System/src/main/java/com/Controller/health/HealthController.java
@@ -1,14 +1,13 @@
-import org.springframework.web.bind.annotation.RequestMapping;
+package controller.health;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController public class HealthController {
+@RestController
+public class HealthController {
 
-   `@RequestMapping("/health")`
-
-            `public String checkAPI(){`
-
-       `return "<h1>The API is working ok!</h1>";`
-
-   `}`
+    @RequestMapping("/health")
+    public String checkAPI() {
+        return "<h1>The API is working ok!</h1>";
+    }
 }
