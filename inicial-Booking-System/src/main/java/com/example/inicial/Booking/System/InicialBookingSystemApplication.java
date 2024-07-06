4b97e65c205a09d79a956a554db5136514c1087d
Cristopher <cristopherquintanilla20060@gmail.com>
2024-07-06 15:16:50 -0600

    configuración inicial Booking System REST API.

---
+++ inicial-Booking-System/src/main/java/com/example/inicial/Booking/System/InicialBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.inicial.Booking.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InicialBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InicialBookingSystemApplication.class, args);
+	}
+
+}
---
+++ inicial-Booking-System/src/test/java/com/example/inicial/Booking/System/InicialBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inicial.Booking.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InicialBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
